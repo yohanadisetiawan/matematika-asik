f17407501ba5c6d32c70edccd13e93fba0655660
yohanadisetiawan <115447721+yohanadisetiawan@users.noreply.github.com>
2022-10-11 09:27:27 +0700

    Basic Activity

---
+++ images.g.ts
@@ -0,0 +1,75 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Dzakir":return img`
+. . . . f f f f . . . . . 
+. . f f f f f f f f . . . 
+. f f f f f f 2 f f f . . 
+f f f f f f 2 2 f f f 2 . 
+f f f 2 f f f f f f f 2 . 
+2 2 2 f f f e e f f 2 2 . 
+f f f f f e e f f 2 2 f . 
+f f f b f e e f b f f f . 
+. f 4 1 f 4 4 f 1 4 f . . 
+. f e 4 4 4 4 4 4 e f . . 
+. f f f e e e e f f f . . 
+f e f b 7 7 7 7 b f e f . 
+e 4 f 7 7 7 7 7 7 f 4 e . 
+e e f 5 5 5 5 5 5 f e e . 
+. . . f f f f f f . . . . 
+. . . f f . . f f . . . . 
+`;
+            case "image2":
+            case "NPC":return img`
+. . . . . f f 4 4 f f . . . . . 
+. . . . f 5 4 5 5 4 5 f . . . . 
+. . . f e 4 5 5 5 5 4 e f . . . 
+. . f b 3 e 4 4 4 4 e 3 b f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. f 3 3 e b 3 e e 3 b e 3 3 f . 
+. f 3 3 f f e e e e f f 3 3 f . 
+. f b b f b f e e f b f b b f . 
+. f b b e 1 f 4 4 f 1 e b b f . 
+f f b b f 4 4 4 4 4 4 f b b f f 
+f b b f f f e e e e f f f b b f 
+. f e e f b d d d d b f e e f . 
+. . e 4 c d d d d d d c 4 e . . 
+. . e f b d b d b d b b f e . . 
+. . . f f 1 d 1 d 1 d f f . . . 
+. . . . . f f b b f f . . . . . 
+`;
+            case "image3":
+            case "NPCLive":return img`
+. . . . . . 5 . 5 . . . . . . . 
+. . . . . f 5 5 5 f f . . . . . 
+. . . . f 1 5 2 5 1 6 f . . . . 
+. . . f 1 6 6 6 6 6 1 6 f . . . 
+. . . f 6 6 f f f f 6 1 f . . . 
+. . . f 6 f f d d f f 6 f . . . 
+. . f 6 f d f d d f d f 6 f . . 
+. . f 6 f d 3 d d 3 d f 6 f . . 
+. . f 6 6 f d d d d f 6 6 f . . 
+. f 6 6 f 3 f f f f 3 f 6 6 f . 
+. . f f d 3 5 3 3 5 3 d f f . . 
+. . f d d f 3 5 5 3 f d d f . . 
+. . . f f 3 3 3 3 3 3 f f . . . 
+. . . f 3 3 5 3 3 5 3 3 f . . . 
+. . . f f f f f f f f f f . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,44 @@
-
+namespace SpriteKind {
+    export const NPC = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC, function (sprite, otherSprite) {
+    mat1 = 0
+    mat2 = 0
+    hasil = 0
+    if (sprite == NPC1) {
+        story.setSoundEnabled(true)
+        mat2 += randint(1, 10)
+        mat2 += randint(0, 10)
+        story.spriteSayText(NPC1, "Berapkah\\n" + mat1 + "+" + mat2, 15)
+        story.showPlayerChoices(convertToText(mat1 + mat2), convertToText(mat1 - mat2))
+        hasil = mat1 + mat2
+        if (story.checkLastAnswer(convertToText(hasil))) {
+            info.changeLifeBy(1)
+            mat1 = 0
+            mat2 = 0
+            hasil = 0
+        }
+    } else {
+        info.changeScoreBy(1)
+        music.baDing.play()
+    }
+    tiles.placeOnRandomTile(NPC1, sprites.dungeon.collectibleRedCrystal)
+    tiles.placeOnRandomTile(NPC2, sprites.dungeon.darkGroundNorthWest1)
+})
+let hasil = 0
+let mat2 = 0
+let mat1 = 0
+let NPC2: Sprite = null
+let NPC1: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+let Pelajar = sprites.create(assets.image`Dzakir`, SpriteKind.Player)
+NPC1 = sprites.create(assets.image`NPC`, SpriteKind.NPC)
+NPC2 = sprites.create(assets.image`NPCLive`, SpriteKind.NPC)
+info.setLife(3)
+info.setScore(0)
+info.startCountdown(120)
+tiles.placeOnRandomTile(Pelajar, sprites.dungeon.collectibleBlueCrystal)
+tiles.placeOnRandomTile(NPC1, sprites.dungeon.collectibleRedCrystal)
+tiles.placeOnRandomTile(NPC2, sprites.dungeon.darkGroundNorthWest1)
+controller.moveSprite(Pelajar, 100, 100)
+scene.cameraFollowSprite(Pelajar)
